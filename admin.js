5d77898a578d6112fe091bdd2f87401996afae70
Joeytex <eletexjoeytex@gmail.com>
2025-04-05 21:15:55 +0100

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,15 @@
+function approveUser() {
+    alert("User approved successfully!");
+}
+
+function blockUser() {
+    alert("User has been blocked!");
+}
+
+function deletePost() {
+    alert("Story deleted successfully.");
+}
+
+function featurePost() {
+    alert("Story marked as featured!");
+}
